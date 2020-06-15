4ebae96872d828f9d57b583ca363c65b8ce38007
Jose Forte <jose.forte@surhive.com>
2020-06-15 19:21:57 -0300

    Add pregnancy module functionality

---
+++ MMM-Pregnancy.js
@@ -0,0 +1,263 @@
+/* global Module */
+
+/* Magic Mirror
+ * Module: MMM-Pregnancy
+ *
+ * By Jose Forte
+ * MIT Licensed.
+ */
+
+Module.register("MMM-Pregnancy", {
+  pregnancyResults: {
+    conception: '',
+    duedate: '',
+    fetalage: {
+      weeks: '',
+      days: ''
+    }
+  },
+  defaults: {
+    header: 'Pregnancy',
+    date: '01/31/2020', // format MM/DD/YYYY
+    USDateFormat: false,
+    showConceptionDate: false
+  },
+
+  getStyles: function() {
+    return ["MMM-Pregnancy.css", "w3.css"];
+  },
+
+  getTranslations: function() {
+    return {
+      en: "translations/en.json",
+      es: "translations/es.json"
+    }
+  },
+
+  start: function() {
+    Log.info("Starting module: " + this.name);
+    
+    this.pregnancyCalc();
+    
+    // Schedule update interval every hour
+    var self = this;
+    setInterval(function() {
+      self.updateDom();
+    }, 3600000)
+  },
+
+  getHeader: function() {
+    return this.config.header;
+  },
+
+  getDom: function() {
+    var moduleWrapper = document.createElement("div");
+    
+    moduleWrapper.appendChild(this.getPregnancyInfo());
+    if (this.config.showConceptionDate) {
+      moduleWrapper.appendChild(this.getConceptionDate());
+    }
+    moduleWrapper.appendChild(this.getDueDate());
+    moduleWrapper.appendChild(this.getProgress());
+
+    return moduleWrapper;
+  },
+
+  getPregnancyInfo: function() {
+    var weekNr = this.pregnancyResults.fetalage.weeks,
+        dayNr = this.pregnancyResults.fetalage.days;
+
+    var counterWrapper = document.createElement("div");
+    var wrapper = document.createElement("table");
+    wrapper.className = "pregnancy-table";
+    
+    counterWrapper.appendChild(wrapper);
+
+    var infoRow = document.createElement("tr"),
+        weeksWrapper = document.createElement("td"),
+        daysWrapper = document.createElement("td");
+   
+    weeksWrapper.innerHTML = weekNr;
+    weeksWrapper.className = 'digits';
+    daysWrapper.innerHTML = dayNr;
+    daysWrapper.className = 'digits';
+
+    infoRow.appendChild(weeksWrapper);
+    infoRow.appendChild(daysWrapper);
+    wrapper.appendChild(infoRow);
+
+    var textsRow = document.createElement("tr"),
+        textWeeksWrapper = document.createElement("td"),
+        textDaysWrapper = document.createElement("td");
+
+    textsRow.className = 'textsRow';
+    textWeeksWrapper.innerHTML = this.translate('WEEKS');
+    textDaysWrapper.innerHTML = this.translate('DAYS');
+
+    textsRow.appendChild(textWeeksWrapper);
+    textsRow.appendChild(textDaysWrapper);
+
+    wrapper.appendChild(textsRow);
+    
+    counterWrapper.className = "pregnancy";
+
+    return counterWrapper;
+  },
+
+  getConceptionDate: function() {
+    var conceptionDateWrapper = document.createElement("div");
+    conceptionDateWrapper.className = 'date';
+
+    conceptionDateWrapper.innerHTML = this.translate('The conception date was') + ' ' + this.pregnancyResults.conception;
+
+    return conceptionDateWrapper;
+  },
+
+  getDueDate: function() {
+    var dueDateWrapper = document.createElement("div");
+    dueDateWrapper.className = 'date';
+
+    dueDateWrapper.innerHTML = this.translate('The due date is') + ' ' + this.pregnancyResults.duedate;
+
+    return dueDateWrapper;
+  },
+
+  getProgress: function() {
+    var t1Bar,
+        t2Bar,
+        t3Bar,
+        weekNr = this.pregnancyResults.fetalage.weeks,
+        trimesterInfo = this.trimesterCalc(weekNr),
+        trimesterTrim = trimesterInfo.trim,
+        trimesterPercent = trimesterInfo.percent;
+
+    if (trimesterTrim == 1) {
+      t1Bar = trimesterPercent; // calculate where of the 1st trimester we are
+      t2Bar = 0; // not yet
+      t3Bar = 0; // not yet
+    } else if (trimesterTrim == 2) {
+      t1Bar = 100; // completed
+      t2Bar = trimesterPercent; // calculate where of the 2nd trimester we are
+      t3Bar = 0; // not yet
+    } else if (trimesterTrim == 3) {
+      t1Bar = 100; // completed
+      t2Bar = 100; // completed
+      t3Bar = trimesterPercent; // calculate where of the 3rd trimester we are
+    }
+    var yearBarCell = document.createElement("div");
+    yearBarCell.className = "pregnancy-timeline__trimesters";
+    yearBarCell.style = "width: 100%;";
+
+    var firstTrimesterContainer = document.createElement("div");
+    firstTrimesterContainer.className = "w3-white";
+    firstTrimesterContainer.style = "float: left; width: 32%; margin-right:2px";
+    var firstTrimesterProgress = document.createElement("div");
+    firstTrimesterContainer.appendChild(firstTrimesterProgress);
+    firstTrimesterProgress.className = "w3-pink";
+    var firstTrimesterText = document.createElement("span");
+    firstTrimesterText.innerHTML = this.translate('1st');
+    firstTrimesterText.style = 'font-size: 25px;color: #ee5486';
+    firstTrimesterProgress.appendChild(firstTrimesterText);
+    firstTrimesterProgress.style = "height:24px;width:" + t1Bar + "%";
+
+    var secondTrimesterContainer = document.createElement("div");
+    secondTrimesterContainer.className = "w3-light-grey";
+    secondTrimesterContainer.style = "float: left; width: 32%; margin-right:2px";
+    
+    var secondTrimesterProgress = document.createElement("div");
+    secondTrimesterContainer.appendChild(secondTrimesterProgress);
+    secondTrimesterProgress.className = "w3-pink";
+    var secondTrimesterText = document.createElement("span");
+    secondTrimesterText.style = 'font-size: 25px;color: #ee5486';
+    secondTrimesterText.innerHTML = this.translate('2nd');
+    secondTrimesterProgress.appendChild(secondTrimesterText);
+    secondTrimesterProgress.style = "height:24px;width:" + t2Bar + "%";
+
+    var thirdTrimesterContainer = document.createElement("div");
+    thirdTrimesterContainer.className = "w3-light-grey";
+    thirdTrimesterContainer.style = "float: left; width: 32%; margin-right:2px";
+    
+    var thirdTrimesterProgress = document.createElement("div");
+    thirdTrimesterContainer.appendChild(thirdTrimesterProgress);
+    thirdTrimesterProgress.className = "w3-pink";
+    var thirdTrimesterText = document.createElement("span");
+    thirdTrimesterText.innerHTML = this.translate('3rd');
+    thirdTrimesterText.style = 'font-size: 25px;color: #ee5486';
+    thirdTrimesterProgress.appendChild(thirdTrimesterText);
+    thirdTrimesterProgress.style = "height:24px;width:"+ t3Bar + "%";
+
+    yearBarCell.appendChild(firstTrimesterContainer);
+    yearBarCell.appendChild(secondTrimesterContainer);
+    yearBarCell.appendChild(thirdTrimesterContainer);
+
+    return yearBarCell;
+  },
+  // calculate which trimester we are and progress percent
+  trimesterCalc: function(weekNr) {
+    var res = {
+      trim: 0,
+      percent: 0
+    }
+    if (weekNr > 0 && weekNr <=13) {
+      res.trim = 1;
+      if (weekNr >= 1 && weekNr <=4 ) res.percent = 25;
+      else if (weekNr >= 5 && weekNr <=8 ) res.percent = 50;
+      else res.percent = 75;
+    } else if (weekNr > 13 && weekNr <=27) {
+      res.trim = 2;
+      if (weekNr >= 14 && weekNr <=17 ) res.percent = 25;
+      else if (weekNr >= 18 && weekNr <=21 ) res.percent = 50;
+      else res.percent = 75;
+    } else if (weekNr > 27) {
+      res.trim = 3;
+      if (weekNr >= 27 && weekNr <=30 ) res.percent = 25;
+      else if (weekNr >= 31 && weekNr <=35 ) res.percent = 50;
+      else res.percent = 75;
+    }
+
+    return res;
+  },
+  // calculate conception date, due date and fetalage (weeks + days)
+  pregnancyCalc: function() {
+    var menstrual = new Date(),
+        ovulation = new Date(),
+        duedate = new Date(),
+        today = new Date(),
+        cycle = 28,
+        luteal = 14;
+    
+    menstrualinput = new Date(this.config.date);
+    menstrual.setTime(menstrualinput.getTime());
+
+    ovulation.setTime(menstrual.getTime() + (cycle*86400000) - (luteal*86400000));
+    var conception = this.dispDate(ovulation);
+    this.pregnancyResults.conception = conception;
+
+    duedate.setTime(ovulation.getTime() + 266*86400000);
+    this.pregnancyResults.duedate = this.dispDate(duedate);
+
+    var fetalage = 14 + 266 - ((duedate - today) / 86400000);
+    weeks = parseInt(fetalage / 7);
+    days = Math.floor(fetalage % 7);
+
+    this.pregnancyResults.fetalage.weeks = weeks;
+    this.pregnancyResults.fetalage.days = days;
+
+    return false;
+  },
+
+  dispDate: function(dateObj) {
+    month = dateObj.getMonth()+1;
+    month = (month < 10) ? "0" + month : month;
+  
+    day   = dateObj.getDate();
+    day = (day < 10) ? "0" + day : day;
+  
+    year  = dateObj.getYear();
+    if (year < 2000) year += 1900;
+    
+    if (this.config.USDateFormat) return (month + "/" + day + "/" + year);
+    else return (day + "/" + month + "/" + year);
+  }
+
+})
--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # MMM-Pregnancy
+A [MagicMirror²](https://magicmirror.builders) helper module to show pregnancy information.
+
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://raw.githubusercontent.com/bibaldo/MMM-CountUP/master/LICENSE)
+
+![Example](.github/example.png)
+
+## Installation
+
+In your terminal, go to your MagicMirror's Module folder:
+````
+cd ~/MagicMirror/modules
+````
+
+Clone this repository:
+````
+git clone https://github.com/bibaldo/MMM-Pregnancy.git
+````
+
+Add the module to the modules array in the `config/config.js` file:
+````javascript
+  {
+    module: "MMM-Pregnancy"
+  },
+````
+
+## Configuration options
+
+The following properties can be configured:
+
+
+| Option                       | Description
+| ---------------------------- | -----------
+| `header`                     | The header text <br><br> **Default value:** `'Pregnancy'`
+| `date`                       | Last menstrual period (LMP) <br> **Format:** `format MM/DD/YYYY`
+| `showConceptionDate`         | Show or not the conception date <br><br>**Default value:** `false`
+| `USDateFormat`               | Show dates with US date format `MM/DD/YYYY` <br><br>**Default value:** `false`
+
+## Config Example
+
+````javascript
+  {
+    module: "MMM-Pregnancy",
+    config: {
+      header: 'Pregnancy',
+      date: '12/20/2019', // format MM/DD/YYYY
+      USDateFormat: false,
+      showConceptionDate: true
+    }
+  },
+````
+## Updating
+
+To update the module to the latest version, use your terminal to go to your MMM-Pregnancy module folder and type the following command:
+
+````
+git pull
+```` 
+
+Feel free to open any Issue :smiley:
